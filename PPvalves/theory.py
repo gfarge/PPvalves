c2a8339d90cb6dd0b93b8ffcd03c26630f813e54
Gaspard Farge <farge@ipgp.fr>
2021-05-07 18:10:29 +0200

    Better docstring for theory.py

--- PPvalves/theory.py
+++ PPvalves/theory.py
@@ -11,12 +11,8 @@ import scipy.special as ssp
 # ====
 
 def calc_trans_ramp(t, X, x1, x2, D):
-    """
-    Computes the solution of the diffusion of a ramp: a linear step from x1 to
-    x2. Before x1, the initial condition is 1, after x2, it's 0.
-
-    Derivation of this solution is presented in Exact_diffusion_solutions
-    notebook.
+    """Computes the solution of the diffusion of a ramp: a linear step from
+    `x1` to `x2`. Before `x1`, the initial condition is 1, after `x2`, it's 0.
 
     Parameters
     ----------
@@ -33,6 +29,7 @@ def calc_trans_ramp(t, X, x1, x2, D):
     -------
     p : 1D array
         Diffusive transient at time t, over X. Same dimension as X.
+
     """
     # Compute transient for step
     # --------------------------
@@ -55,7 +52,31 @@ def calc_trans_ramp(t, X, x1, x2, D):
 
 
 def Perf(X, T, X0, D, dP):
-    """ Calculate the P error function for all times on the whole domain (inf domain)"""
+    """Computes diffusion of pressure step in time (infinite domain : erf
+    function)
+
+    Parameters
+    ----------
+    X : 1D array
+        Space array to compute the solution on.
+    T : 1D array
+        Time vector.
+    X0 : float
+        Step location.
+    L : float
+        The domain extent in space.
+    D : float
+        Diffusivity coefficient.
+    dP : float
+        Step amplitude.
+
+    Returns
+    -------
+    erfP: 2D array
+        The theoretical solution for bounded diffusion, dimensions `(Ntime,
+        Nspace)`.
+
+    """
 
     print('Perf -- Computing infinite domain solution...')
     erfP = np.zeros((len(T),len(X)))
@@ -72,9 +93,9 @@ def Perf(X, T, X0, D, dP):
 
 
 
-def Perf_bounded(X,T,X0,L,D,dP,n=5):
-    """
-    Computes the solution for Pp in a bounded domain, with no-flux boundaries.
+def Perf_bounded(X, T, X0, L, D, dP, n=5):
+    """Computes the diffusion of a step in a bounded domain, with no-flux
+    boundaries.
 
     To cancel the flux at each boundary, the domain is considered cyclic and
     infinite with mirrored pressure across each boundary.
@@ -82,28 +103,39 @@ def Perf_bounded(X,T,X0,L,D,dP,n=5):
     http://web.mit.edu/1.061/www/dream/FOUR/FOURTHEORY.PDF
     http://www.dartmouth.edu/~cushman/courses/engs43/Diffusion-variations.pdf
 
-    - Parameters
-    	+ :param X: the space domain (numpy array 1D)
-    	+ :param T: time vector (numpy array 1D)
-    	+ :param X0: step location (float)
-    	+ :param L: the domain extent in space (float).
-    	+ :param D: diffusivity coefficient (float)
-    	+ :param dP: step amplitude (float)
-    	+ :param n: number of mirror solutions, keep odd, less than 7 is always
-    	enough.
-    - Outputs
-    	+ :return: erfP: numpy array of the theoretical solution for bounded
-    	diffusion.
+    Parameters
+    ----------
+    X : 1D array
+        Space array to compute the solution on.
+    T : 1D array
+        Time vector.
+    X0 : float
+        Step location.
+    L : float
+        The domain extent in space.
+    D : float
+        Diffusivity coefficient.
+    dP : float
+        Step amplitude.
+    n : interger
+        Number of mirror solutions to compute no-flux boundary, keep odd, less
+        than 7 is always enough.
+
+    Returns
+    -------
+    erfP: 2D array
+        The theoretical solution for bounded diffusion, dimensions `(Ntime,
+        Nspace)`.
 
     """
     print('Perf_bounded -- Computing no-flux boundaries solution...')
     m = n//2
     if m > 0:
-    	sign_x = np.ones(m)
-    	sign_x[::2] = -1
+        sign_x = np.ones(m)
+        sign_x[::2] = -1
 
-    erfP = np.zeros((len(T),len(X)))
-    for itt,tt in enumerate(T):
+    erfP = np.zeros((len(T), len(X)))
+    for itt, tt in enumerate(T):
         v = (X - X0) / np.sqrt(4*D*tt)
         erfP[itt,:] = dP/2. * ssp.erf(v)
         if m > 0:
